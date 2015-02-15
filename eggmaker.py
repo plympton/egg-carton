3ba70f5ce5234bfb83ca4c8edc8ccab17941647e
Alexandra Murray <horisustar@gmail.com>
2015-02-15 15:39:35 -0500

    Now counts words!

---
+++ eggmaker.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+from os import path
+
+#load stuff into bsoup
+#take command from the command line, arg-v
+
+#load var
+#load var into soup
+#run soup commands on var
+
+soup = BeautifulSoup(open("the-gathering-0.html"))
+
+#print(soup.find_all("p"))
+
+wordCount = 0
+
+for p in soup.find_all("p"):
+    words = p.get_text()
+    #print words
+    words = words.split()
+    #type(thing)
+    wordCount +=len(words)
+    print wordCount
